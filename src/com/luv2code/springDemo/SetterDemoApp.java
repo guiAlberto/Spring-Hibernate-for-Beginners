e519cfc35ffc6e5f7dcfc3ff55f37045be86d2c3
Guilherme Alberto de Moraes <g.alberto.moraes@gmail.com>
2018-07-25 00:43:16 -0300

    Section 5: Spring Dependency Injection - XML Configuration

--- src/com/luv2code/springDemo/SetterDemoApp.java
+++ src/com/luv2code/springDemo/SetterDemoApp.java
@@ -2,21 +2,23 @@ package com.luv2code.springDemo;
 
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
-public class HelloSpringApp {
+public class SetterDemoApp {
 
 	public static void main(String[] args) {
-
 		// load the spring configuration file
-		ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
+		ClassPathXmlApplicationContext context = 
+				new ClassPathXmlApplicationContext("applicationContext.xml");
 		
 		// retrieve bean from spring container
-		Coach theCoach = context.getBean("myCoach", Coach.class);
+		CricketCoach theCoach = context.getBean("myCricketCoach", CricketCoach.class);
 		
-		// call methods on bean
+		// call methods from bean
 		System.out.println(theCoach.getDailyWorkout());
-		
-		// let's call our method for fortunes
 		System.out.println(theCoach.getDailyFortune());
+		
+		// call out methods to get literal values
+		System.out.println(theCoach.getEmailAddress());
+		System.out.println(theCoach.getTeam());
 		
 		// close the context
 		context.close();
---
+++ src/com/luv2code/springDemo/exercises/ExerciseApp.java
@@ -0,0 +1,15 @@
+package com.luv2code.springDemo.exercises;
+
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class ExerciseApp {
+
+	public static void main(String[] args) {
+		ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("com/luv2code/springDemo/exercises/exercicesApplicationContext.xml");
+		SwimmingCoach theCoach = context.getBean("mySwimmingCoach", SwimmingCoach.class);
+		System.out.println(theCoach.getDailyWorkout());
+		System.out.println(theCoach.getDailyFortune());
+		context.close();
+	}
+
+}
