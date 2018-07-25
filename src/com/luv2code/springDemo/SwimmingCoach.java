421a02afeacedc9cd20b32c9883a72bea6a16e88
Guilherme Alberto de Moraes <g.alberto.moraes@gmail.com>
2018-07-25 00:15:23 -0300

    Section 4: Spring Inversion of Control - XML Configuration

---
+++ src/com/luv2code/springDemo/SwimmingCoach.java
@@ -0,0 +1,12 @@
+package com.luv2code.springDemo;
+
+public class SwimmingCoach implements Coach {
+
+	@Override
+	public String getDailyWorkout() {
+		
+		return "Swim from Tokyo to Shanghai!";
+		
+	}
+
+}
