1267c84ee137cbbd445075bc7cb0b9c6f39a9f0e
Guilherme Alberto de Moraes <g.alberto.moraes@gmail.com>
2018-08-08 17:29:06 -0300

    Section 6: Spring Bean Scopes and Lifecycle

--- src/com/luv2code/springDemo/TrackCoach.java
+++ src/com/luv2code/springDemo/TrackCoach.java
@@ -22,5 +22,15 @@ public class TrackCoach implements Coach {
 	public String getDailyFortune() {
 		return "Just Do It: " + fortuneService.getFortune();
 	}
+	
+	// add an init method
+	public void doMyStartupStuff(){
+		System.out.println("TrackCoach: inside method doMyStartupStuff");
+	}
+	
+	// add an destroy method
+	public void doMyCleanUpStuffYoYo(){
+		System.out.println("TrackCoach: inside method doMyCleanUpStuffYoYo");
+	}
 
 }
--- src/com/luv2code/springDemo/exercises/ExerciseApp.java
+++ src/com/luv2code/springDemo/exercises/ExerciseApp.java
@@ -7,9 +7,14 @@ public class ExerciseApp {
 	public static void main(String[] args) {
 		ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext(
 				"com/luv2code/springDemo/exercises/exercicesApplicationContext.xml");
-		SwimmingCoach theCoach = context.getBean("mySwimmingCoach", SwimmingCoach.class);
-		System.out.println(theCoach.getDailyWorkout());
-		System.out.println(theCoach.getDailyFortune());
+		SwimmingCoach coachA = context.getBean("mySwimmingCoachSingleton", SwimmingCoach.class);
+		SwimmingCoach coachB = context.getBean("mySwimmingCoachSingleton", SwimmingCoach.class);
+		SwimmingCoach coachC = context.getBean("mySwimmingCoachPrototype", SwimmingCoach.class);
+		SwimmingCoach coachD = context.getBean("mySwimmingCoachPrototype", SwimmingCoach.class);
+
+		System.out.println("coachA == coachB: " + (coachA == coachB));
+		System.out.println("coachC == coachD: " + (coachC == coachD));
+
 		context.close();
 	}
 
